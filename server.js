8b621a184f49ff2c7ba4aedf5926f251f7f3858e
pollys4xy <work.r4sty@gmail.com>
2025-05-23 00:51:24 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -4,17 +4,12 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware: анализируем User-Agent
 app.use((req, res, next) => {
   const ua = req.headers['user-agent'] || '';
-  const isTiktok = /tiktok/i.test(ua);
-  const isInstagram = /instagram/i.test(ua);
-  const isFacebook = /fbav/i.test(ua);
-
-  if (!isTiktok && !isInstagram && !isFacebook) {
+  const isInApp = /tiktok|instagram|fbav|facebook/i.test(ua);
+  if (!isInApp) {
     return res.redirect('https://getallmylinks.com/pollys4xy');
   }
-
   next();
 });
 
